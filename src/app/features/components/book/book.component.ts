702fa665dafddbf12d27985e0f46314e6703c508
cretara <pierdanielecretara@lviiier.com>
2024-10-26 11:11:37 +0200

    Added spinner in book.component

--- src/app/features/components/book/book.component.ts
+++ src/app/features/components/book/book.component.ts
@@ -1,16 +1,19 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import {BooksStore} from '../../../store/book-store';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, MatProgressSpinner],
   templateUrl: './book.component.html',
   providers: [BooksStore],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BooksComponent implements OnInit {
   readonly bookStore = inject(BooksStore);
+
+  public loading = this.bookStore.isLoading;
 
   ngOnInit(): void {
     console.debug('BooksComponent initialized');
